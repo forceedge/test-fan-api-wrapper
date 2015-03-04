d906929f5f69e4f542c2a1b21f475fcf350810e1
Wahab Qureshi <Wahab.Qureshi@digitalanimal.com>
2015-03-04 14:22:27 +0000

    Removed key n secret

--- index.php
+++ index.php
@@ -6,8 +6,8 @@ error_reporting(~0);
 function getInstance() {
 	require_once __DIR__ . '/Fandist.Api.Wrapper/Src/Core/Connector.php';
 
-	$apiKey = 'c14nEtiH98g=';
-	$apiSecret = 'fb6c93758c14c7aac1fc159190e6bf2b045e10d871ef2fd3a7b179e02fe8a8e81c0f105dd462a3bb905bfe711044df5036cdad67e750f476e31f2a54d53c2f33';
+	$apiKey = 'YOUR-APP-KEY';
+	$apiSecret = 'YOUR-APP-SECRET';
 
 	return \Src\Core\Connector::getInstance($apiKey, $apiSecret);
 }
